472961f9ea1cd6733ce534d1bb5191936d658500
sabababluani <158265615+sabababluani@users.noreply.github.com>
2024-07-09 20:41:57 +0400

    feat: added Button in components file

--- app/Components/Button/Button.tsx
+++ app/Components/Button/Button.tsx
@@ -1,12 +1,27 @@
 import styles from './Button.module.scss';
 
+export enum Mode {
+    Fill = 'fill',
+    Outline = 'outline',
+    Inline = 'inline',
+    Delete = 'delete',
+    Transparent = 'transparent',
+    More = 'more',
+}
+
+export enum ButtonType {
+    Text = 'text',
+    IconText ='icon-text',
+    IconOnly ='icon-Only',
+}
+
 type Props = {
     title: string;
-    mode?: 'fill' | 'outline' | 'inline' | 'delete' | 'transparent' | 'more';
+    mode?: Mode;
     disabled?: boolean;
     onClick?: () => void;
     icon?: string;
-    type?: 'text' | 'icon-only' | 'icon-text';
+    type?: ButtonType;
 }
 
 const Button = (props: Props) => {
@@ -14,16 +29,16 @@ const Button = (props: Props) => {
     const classes = [styles.container];
     const iconClass = [];
 
-    if (props.mode === 'fill') classes.push(styles.fill);
-    if (props.mode === 'outline') classes.push(styles.outline);
-    if (props.mode === 'more') classes.push(styles.more);
+    if (props.mode === Mode.Fill) classes.push(styles.fill);
+    if (props.mode === Mode.Outline) classes.push(styles.outline);
+    if (props.mode === Mode.More) classes.push(styles.more);
 
-    if (props.type === 'icon-text' && props.icon) iconClass.push(styles.iconContainer);
-    if (props.type === 'icon-only' && props.icon) {
-        if (props.mode === 'inline') iconClass.push(styles.inline);
-        if (props.mode === 'delete') iconClass.push(styles.delete);
-        if (props.mode === 'transparent') iconClass.push(styles.transparent);
-    }
+    if (props.type === ButtonType.IconText && props.icon) iconClass.push(styles.iconContainer);
+    if (props.type === ButtonType.IconOnly&& props.icon) {
+        if (props.mode === Mode.Inline) iconClass.push(styles.inline);
+        if (props.mode === Mode.Delete) iconClass.push(styles.delete);
+        if (props.mode === Mode.Transparent) iconClass.push(styles.transparent);
+    };
 
     return (
         <button
@@ -31,16 +46,16 @@ const Button = (props: Props) => {
             disabled={props.disabled}
             className={props.icon ? iconClass.join(' ').trim() : classes.join(' ').trim()}
         >
-            {props.type === 'icon-only' && props.icon && (
+            {props.type === ButtonType.IconOnly && props.icon && (
                 <img src={props.icon} alt='Icon' />
             )}
-            {props.type === 'icon-text' && props.icon && (
+            {props.type === ButtonType.IconText && props.icon && (
                 <>
                     <img src={props.icon} alt='Icon' className={styles.icon} />
                     {props.title}
                 </>
             )}
-            {props.type === 'text' && props.title}
+            {props.type === ButtonType.Text && props.title}
         </button>
     );
 }
