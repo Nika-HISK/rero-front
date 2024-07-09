88ea5cb7c2962b370a68d9120ec76e95b0c2de49
sabababluani <158265615+sabababluani@users.noreply.github.com>
2024-07-09 20:25:33 +0400

    feat: added button in components

--- app/Components/Button/Button.tsx
+++ app/Components/Button/Button.tsx
@@ -2,10 +2,11 @@ import styles from './Button.module.scss';
 
 type Props = {
     title: string;
-    mode?: 'fill' | 'outline';
+    mode?: 'fill' | 'outline' | 'inline' | 'delete' | 'transparent' | 'more';
     disabled?: boolean;
     onClick?: () => void;
     icon?: string;
+    type?: 'text' | 'icon-only' | 'icon-text';
 }
 
 const Button = (props: Props) => {
@@ -14,8 +15,15 @@ const Button = (props: Props) => {
     const iconClass = [];
 
     if (props.mode === 'fill') classes.push(styles.fill);
-    else classes.push(styles.outline);
-    if (props.icon) iconClass.push(styles.iconContainer);
+    if (props.mode === 'outline') classes.push(styles.outline);
+    if (props.mode === 'more') classes.push(styles.more);
+
+    if (props.type === 'icon-text' && props.icon) iconClass.push(styles.iconContainer);
+    if (props.type === 'icon-only' && props.icon) {
+        if (props.mode === 'inline') iconClass.push(styles.inline);
+        if (props.mode === 'delete') iconClass.push(styles.delete);
+        if (props.mode === 'transparent') iconClass.push(styles.transparent);
+    }
 
     return (
         <button
@@ -23,12 +31,18 @@ const Button = (props: Props) => {
             disabled={props.disabled}
             className={props.icon ? iconClass.join(' ').trim() : classes.join(' ').trim()}
         >
-            {props.icon && (
-                <img src={props.icon} alt='Icon' className={styles.icon} />
+            {props.type === 'icon-only' && props.icon && (
+                <img src={props.icon} alt='Icon' />
             )}
-            {props.title}
+            {props.type === 'icon-text' && props.icon && (
+                <>
+                    <img src={props.icon} alt='Icon' className={styles.icon} />
+                    {props.title}
+                </>
+            )}
+            {props.type === 'text' && props.title}
         </button>
-    )
+    );
 }
 
 export default Button;
