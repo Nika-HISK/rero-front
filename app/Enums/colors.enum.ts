55e35a5c2d9e39ac38327f6df72a30abf22eef6a
sabaabramishvili <sabramashvili02@gmail.com>
2024-07-11 12:51:51 +0400

    feat: create reusable color component

---
+++ app/Enums/colors.enum.ts
@@ -0,0 +1,11 @@
+enum colors {
+    primary_Color = '#052224',
+    primary_Hover = '#13828A',
+    primary_Text = '#FFFFFF',
+    secondary_Text = '#121212',
+    third_Text = '#13828A',
+    additional_Bg = '#FF4343',
+    primary_Linear = 'linear - gradient(225deg, #052224 0 %, #13828A 97.09 %)',
+}
+
+export default colors
