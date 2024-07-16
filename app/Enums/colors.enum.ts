ed0b90db8c874c5f65069a08f43dc2c0729f1652
sabaabramishvili <sabramashvili02@gmail.com>
2024-07-16 15:56:05 +0400

    fix: correct names of enums and change export syntax

--- app/Enums/colors.enum.ts
+++ app/Enums/colors.enum.ts
@@ -1,11 +1,10 @@
-enum colors {
-    primary_Color = '#052224',
-    primary_Hover = '#13828A',
-    primary_Text = '#FFFFFF',
-    secondary_Text = '#121212',
-    third_Text = '#13828A',
-    additional_Bg = '#FF4343',
-    primary_Linear = 'linear - gradient(225deg, #052224 0 %, #13828A 97.09 %)',
+export enum colors {
+    Primary_Color = '#052224',
+    Primary_Hover = '#13828A',
+    Primary_Text = '#FFFFFF',
+    Secondary_Text = '#121212',
+    Third_Text = '#13828A',
+    Additional_Bg = '#FF4343',
+    Primary_Linear = 'linear - gradient(225deg, #052224 0 %, #13828A 97.09 %)',
 }
 
-export default colors
