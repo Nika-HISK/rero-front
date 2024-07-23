5d9dc3f508f1e60a56dd68ef4af37e096f2dbec6
sabaabramishvili <sabramashvili02@gmail.com>
2024-07-23 14:00:28 +0400

    feat: create icons enum

---
+++ app/Enums/Icons.enum.ts
@@ -0,0 +1,9 @@
+export enum IconsEnum {
+    add = '../icons/add.svg',
+    addplaylist = '../icons/addplaylist.svg',
+    edit = '../icons/edit.svg',
+    home = '../icons/home.svg',
+    miniadd = '../icons/miniadd.svg',
+    play = '../icons/play.svg',
+    playlist = '../icons/playlist.svg'
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { IconsEnum } from "./Enums/Icons.enum";
 
 export default function Home() {
   return (
     <>
-  
     </>
   );
 }
