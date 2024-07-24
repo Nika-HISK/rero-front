3074afbebdc25a9c45017fbc5643155e9c419293
sabaabramishvili <sabramashvili02@gmail.com>
2024-07-24 14:43:38 +0400

    refactor: change BaseApi sturcture

--- app/api/BaseApi.ts
+++ app/api/BaseApi.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3001';
+const BaseApi = axios.create({
+    baseURL: 'http://localhost:3001',
+  });
 
-export default axios;
+export default BaseApi;
