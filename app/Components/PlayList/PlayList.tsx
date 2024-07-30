796bf9ace24c99ce4c8f70e0554e90295528ec7d
alexandre879 <aleksiapkhazava@gmail.com>
2024-07-30 18:38:43 +0400

    feat/PROFE-37/create-playlist-row-component

---
+++ app/Components/PlayList/PlayList.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import styles from './PlayList.module.scss';
+import { useState } from 'react';
+
+interface Props {
+    playlistName: string;
+    artistName?: string;
+    artistPhoto: string;
+    musicName?: string;
+}
+
+const PlayList = (props: Props) => {
+
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    const onChangeToggle = () => {
+        setIsOpen((prev) => !prev);
+    }
+
+    return (
+        <>
+            <div className={styles.playListWrapper} >
+                <div className={styles.wrapper} >
+                    <img className={styles.artistCover} src={props.artistPhoto} alt={`${props.playlistName} cover`} />
+                    <span className={styles.playlistName}>{props.playlistName}</span>
+                    <img onClick={onChangeToggle} src={isOpen ? '/playlist/down.svg' : '/playlist/up.svg'} alt="Toggle" />
+                </div>
+                <div className={`${styles.afterOpen} ${!isOpen ? styles.collapsed : ''}`} >
+                    <div>
+                        <div className={styles.looperShuffleWrapper} >
+                            <img src='/playlist/looper.svg' alt="Looper" />
+                            <img src='/playlist/shuffle.svg' alt="Shuffle" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default PlayList;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import PlayList from "./Components/PlayList/PlayList";
+import { Play } from "next/font/google";
 
 export default function Home() {
   return (
     <>
-  
+      
     </>
   );
 }
