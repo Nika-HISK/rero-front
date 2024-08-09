48a0938bb2411eb4777fd2dab8885692c5120b4e
sabababluani <158265615+sabababluani@users.noreply.github.com>
2024-08-09 16:27:14 +0400

    feat: created artist page

--- app/Components/ArtistCard/ArtistCard.tsx
+++ app/Components/ArtistCard/ArtistCard.tsx
@@ -4,23 +4,21 @@ import styles from './ArtistCard.module.scss';
 import { ArtistCardPropsInterface } from './interfaces/artist-card-props.interface';
 
 const ArtistCard = (props: ArtistCardPropsInterface) => {
-  // TODO: add href in Link
+  //TODO: Link href 
   return (
-    <div className={styles.wrapper}>
-      <Link href="">
-        <div className={styles.container}>
-          <Image
-            src={props.artistPhoto}
-            alt="artistPhoto"
-            width={158}
-            height={305}
-          />
-        </div>
-        <div className={styles.containerParagraph}>
-          <p>{props.artistName}</p>
-        </div>
-      </Link>
-    </div>
+    <Link href="" className={styles.wrapper}>
+      <div className={styles.container}>
+        <Image
+          src={props.artistPhoto}
+          alt={props.artistName}
+          width={158}
+          height={305}
+        />
+      </div>
+      <div className={styles.containerParagraph}>
+        <p>{props.artistName}</p>
+      </div>
+    </Link>
   );
 };
 
