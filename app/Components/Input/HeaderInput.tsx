30357c24aeb66e5bd26feaf88c5da1e99b658bb3
alexandre879 <aleksiapkhazava@gmail.com>
2024-08-12 00:37:27 -0700

    feat: created reusable input component

--- app/Components/Input/HeaderInput.tsx
+++ app/Components/Input/HeaderInput.tsx
@@ -1,44 +1,55 @@
 'use client';
 
-import styles from "./Input.module.scss";
-import React, { useState, useEffect, useRef } from "react";
-import { InputPropsInterface } from "./interfaces/input-props.interface";
+import styles from './Input.module.scss';
+import React, { useState, useEffect, useRef } from 'react';
+import { InputPropsInterface } from './interfaces/input-props.interface';
 
 const HeaderInput = (props: InputPropsInterface) => {
-    const [isActive, setIsActive] = useState<boolean>(false);
-    const inputRef = useRef<HTMLDivElement>(null);
+  const [isActive, setIsActive] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
 
-    const toggleClass = () => {
-        setIsActive(!isActive);
+  const toggleClass = () => {
+    setIsActive(!isActive);
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      containerRef.current &&
+      !containerRef.current.contains(event.target as Node)
+    ) {
+      setIsActive(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
     };
+  }, []);
 
-    const handleClickOutside = (event: MouseEvent) => {
-        if (inputRef.current && !inputRef.current.contains(event.target as Node)) {
-            setIsActive(false);
-        }
-    };
+  useEffect(() => {
+    if (isActive && window.innerWidth <= 1024) {
+      inputRef.current?.focus();
+    }
+  }, [isActive]);
 
-    useEffect(() => {
-        document.addEventListener("mousedown", handleClickOutside);
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, []);
-
-    return (
-        <div ref={inputRef} className={styles.mainContainer}>
-            <input
-                className={`${styles.inputStyle} ${isActive ? '' : styles.isActiveInput}`}
-                type="text"
-                placeholder={`       ${props.placeholder}`}
-            />
-            <img
-                className={`${styles.imgStyle} ${isActive && styles.isActive}`}
-                onClick={toggleClass}
-                src="/searchIcon/search.svg"
-            />
-        </div>
-    );
+  return (
+    <div ref={containerRef} className={styles.mainContainer}>
+      <input
+        ref={inputRef}
+        className={`${styles.inputStyle} ${isActive ? '' : styles.isActiveInput}`}
+        type="text"
+        placeholder={`       ${props.placeholder}`}
+      />
+      <img
+        className={`${styles.imgStyle} ${isActive && styles.isActive}`}
+        onClick={toggleClass}
+        src="/searchIcon/search.svg"
+      />
+    </div>
+  );
 };
 
 export default HeaderInput;
