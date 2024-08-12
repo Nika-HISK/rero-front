badc8e40d5932cd24f8da2baf5a46940c7a43af3
alexandre879 <aleksiapkhazava@gmail.com>
2024-08-12 00:43:03 -0700

    feat: created reusable header input component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import React from "react";
-import Input from "./Components/HeaderInput/HeaderInput";
-import HeaderInput from "./Components/HeaderInput/HeaderInput";
+import Image from 'next/image';
+import styles from './page.module.css';
+import React from 'react';
+import Input from './Components/HeaderInput/HeaderInput';
+import HeaderInput from './Components/HeaderInput/HeaderInput';
 
 export default function Home() {
-  return (
-    <>
-      <HeaderInput placeholder={"Type something: "} />
-    </>
-  );
+  return <></>;
 }
