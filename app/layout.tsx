da1f3b0565772b88e273a6a3cedf5ac07900f27c
sabababluani <158265615+sabababluani@users.noreply.github.com>
2024-08-13 21:21:48 +0400

    feat: created artist page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Header from "./Components/Header/Header";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import RecoilWrapper from './Components/RecoilWrapper/RecoilWrapper';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -21,8 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} children`}>
-        <Header />
-        {children}
+        <RecoilWrapper>{children}</RecoilWrapper>
       </body>
     </html>
   );
