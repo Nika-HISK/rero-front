bb7bf39ec18a6389d7a13feabd296001cdac67c9
sabaabramishvili <sabramashvili02@gmail.com>
2024-08-13 17:44:52 +0400

    feat: create header component

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import styles from './page.module.css';
 import React from 'react';
 import Input from './Components/HeaderInput/HeaderInput';
 import HeaderInput from './Components/HeaderInput/HeaderInput';
+import Header from './Components/Header/Header';
 
 export default function Home() {
-  return <></>;
+  return <>
+  <Header />
+  </>;
 }
