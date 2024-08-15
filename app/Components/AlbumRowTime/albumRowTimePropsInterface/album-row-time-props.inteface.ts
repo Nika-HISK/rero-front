546b92898550677d20c1151d7dd07d7fb876d6e2
sabababluani <158265615+sabababluani@users.noreply.github.com>
2024-08-15 20:17:50 +0400

    feat: created playlist row component

---
+++ app/Components/AlbumRowTime/albumRowTimePropsInterface/album-row-time-props.inteface.ts
@@ -0,0 +1,8 @@
+export interface AlbumRowTimePropsInterface {
+  src: string;
+  musicName: string;
+  artistName: string;
+  albumName: string;
+  createTime: string;
+}
+
--- app/Components/PlayList/playlistPropsInterface/playlistPropsInterface.ts
+++
@@ -1,6 +0,0 @@
-export interface Props {
-    playlistName: string;
-    artistName?: string;
-    artistPhoto: string;
-    musicName?: string;
-}
