02281f79d67efbe8866e4343673e5f48d7b2c8e1
sabababluani <158265615+sabababluani@users.noreply.github.com>
2024-08-16 17:14:06 +0400

    feat: created top artists page

---
+++ app/topartists/components/ArtistCardBelowName/ArtistCardBelowName.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import styles from './ArtistCardBelowName.module.scss';
+import { ArtistCardBelowNamePropsInterface } from './artist-card-below-name-props.interface/artist-card-below-name-props.interface';
+import Link from 'next/link';
+
+const ArtistCardBelowName = (props: ArtistCardBelowNamePropsInterface) => {
+    //TODO Link Href 
+  return (
+    <Link className={styles.wrapper} href=''>
+      <Image
+        src={props.artistPhoto}
+        width={392}
+        height={264}
+        alt="artistPhoto"
+      />
+      <div className={styles.container}>
+        <span>{props.artistName}</span>
+      </div>
+    </Link>
+  );
+};
+
+export default ArtistCardBelowName;
