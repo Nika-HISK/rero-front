2dc1059f791f7276abf9a1d22ed1db13427e4dcd
sabababluani <Saba.bablo@gmail.com>
2024-08-23 00:49:54 +0400

    feat: created playlist pop up component

--- app/Components/SelectPlaylistPopUp/SelectPlaylistPopUp.tsx
+++ app/Components/SelectPlaylistPopUp/SelectPlaylistPopUp.tsx
@@ -1,51 +1,60 @@
-import styles from "./SelectPlaylistPopUp.module.scss";
-import { useState } from "react";
-import { Props } from "./interface/Select-playlist-popup-interface";
-import { albumsSelectPupUpDummy } from "./select-playlist-dummy-data/dummy-data";
-import { ItemType } from "./select-playlist-dummy-data/dummy-data";
-import Button from "../Button/Button";
-import { ButtonMode } from "@/app/Enums/ButtonMode.enum";
-import { ButtonType } from "@/app/Enums/ButtonType.enum";
+import styles from './SelectPlaylistPopUp.module.scss';
+import { useState } from 'react';
+import { Props } from './interface/Select-playlist-popup-interface';
+import { albumsSelectPupUpDummy } from './select-playlist-dummy-data/dummy-data';
+import { ItemType } from './select-playlist-dummy-data/dummy-data';
+import Button from '../Button/Button';
+import { ButtonMode } from '@/app/Enums/ButtonMode.enum';
+import { ButtonType } from '@/app/Enums/ButtonType.enum';
 
 const SelectPlaylistPopUp = (props: Props) => {
+  const [value, setValue] = useState<string | undefined>(undefined);
 
-    const [value, setValue] = useState<string | undefined>(undefined);
+  const onChangeValue = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setValue(e.target.value);
+    props.value(true);
+  };
 
-    const onChangeValue = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        setValue(e.target.value);
-        props.value(true)
-    }
-
-    console.log(props.option)
-
-    return (
-        <div className={styles.mainWrapper} >
-            <div className={styles.wrapper} >
-                <div className={styles.captionAndSelectWrapper} >
-                    <span className={styles.captionStyle} >Select Playlist</span>
-                    <div className={styles.selectStyleWrapper} >
-                        <select className={styles.selectStyle} value={value} onChange={onChangeValue} >
-                            {albumsSelectPupUpDummy.map((item: ItemType, index: number) => (
-                                <option key={index + 1} className={styles.optionStyle}>
-                                    {item.name}
-                                </option>
-                            ))}
-                        </select>
-                    </div>
-                </div>
-                <div className={styles.buttonsWrapper} >
-                    <div className={styles.cancelButtonWrapper} >
-                        <Button mode={ButtonMode.Fill} title="Cancel" type={ButtonType.Text} onClick={() => props.value(!props.option)} />
-                    </div>
-                    <div className={styles.confirmButtonWrapper} >
-                        {props.option ?
-                            <Button title="Confirm" mode={ButtonMode.Fill} type={ButtonType.Text}  disabled /> :
-                            <Button mode={ButtonMode.Fill} title="Confirm" type={ButtonType.Text} />}
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className={styles.mainWrapper}>
+      <div className={styles.wrapper}>
+        <div className={styles.captionAndSelectWrapper}>
+          <span className={styles.captionStyle}>Select Playlist</span>
+          <div className={styles.selectStyleWrapper}>
+            <select
+              className={styles.selectStyle}
+              value={value}
+              onChange={onChangeValue}
+            >
+              {albumsSelectPupUpDummy.map((item: ItemType, index: number) => (
+                <option key={index} className={styles.optionStyle}>
+                  {item.name}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
-    );
-}
+        <div className={styles.buttonsWrapper}>
+          <div className={styles.cancelButtonWrapper}>
+            <Button
+              mode={ButtonMode.Fill}
+              title="Cancel"
+              type={ButtonType.Text}
+              onClick={() => props.value(false)}
+            />
+          </div>
+          <div className={styles.confirmButtonWrapper}>
+            <Button
+              mode={ButtonMode.Fill}
+              title="Confirm"
+              type={ButtonType.Text}
+              disabled={!value} 
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SelectPlaylistPopUp;
