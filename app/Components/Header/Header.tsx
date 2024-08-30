928ac7e480bd8cbb112ee74e308ba7bde2b62d0d
sabaabramishvili <sabramashvili02@gmail.com>
2024-08-30 14:03:11 +0400

    refactor: change icon sturcture

--- app/Components/Header/Header.tsx
+++ app/Components/Header/Header.tsx
@@ -1,10 +1,13 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
 import HeaderInput from '../HeaderInput/HeaderInput';
 import Icon from '../Icons/Icon';
 import styles from './Header.module.scss';
+import { usePathname } from 'next/navigation';
 
 const Header = () => {
+  const pathname = usePathname();
   return (
     <div className={styles.fullscreenContainer}>
       <div className={styles.container}>
@@ -21,9 +24,19 @@ const Header = () => {
             <HeaderInput placeholder={'Search'} />
           </div>
           <Link className={styles.homeButton} href="/">
-            <Icon name={'home'} width={21} height={21} />
+            {pathname == '/' ? (
+              <Icon name={'home'} width={48} height={48} isActive />
+            ) : (
+              <Icon name={'home'} width={21} height={21} />
+            )}
           </Link>
-          <Icon name={'playlist'} width={21} height={21} />
+          <Link href={'/playlist'}>
+            {pathname == '/playlist' ? (
+              <Icon name={'playlist'} width={48} height={48} isActive />
+            ) : (
+              <Icon name={'playlist'} width={21} height={21} />
+            )}
+          </Link>
           <Icon name={'exit'} width={21} height={21} />
         </div>
       </div>
