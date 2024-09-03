e47defac3ea5ce46b2a851f0d9691cd980fe90af
sabaabramishvili <sabramashvili02@gmail.com>
2024-09-03 20:04:11 +0400

    feat/implement axios interceptors

--- app/api/BaseApi.ts
+++ app/api/BaseApi.ts
@@ -4,4 +4,32 @@ const BaseApi = axios.create({
   baseURL: 'https://back.reroapp.ge',
 });
 
+// Request Interceptor
+BaseApi.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+BaseApi.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      window.location.href = '/login';
+    }
+
+    return Promise.reject(error);
+  },
+);
+
 export default BaseApi;
