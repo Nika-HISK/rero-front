c86db3c706e10d5f9146bd2fc829ee1f9365f316
sabaabramishvili <sabramashvili02@gmail.com>
2024-09-05 13:53:36 +0400

    refactor: setting token in localstorage

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -32,8 +32,10 @@ const Login = () => {
     }
 
     BaseApi.post('/auth/login', data)
-      .then(() => {
+      .then((response) => {
         router.push('/');
+        localStorage.setItem('token', response.data.accessToken)
+        
       })
       .catch((error) => {
         alert(error.response.data.message);
