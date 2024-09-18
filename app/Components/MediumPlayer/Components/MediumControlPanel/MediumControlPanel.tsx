9696cf2bc8037fe363649208f02a424d9e38e674
sabaabramishvili <sabramashvili02@gmail.com>
2024-09-18 14:50:34 +0400

    fix: add post to shuffle

--- app/Components/MediumPlayer/Components/MediumControlPanel/MediumControlPanel.tsx
+++ app/Components/MediumPlayer/Components/MediumControlPanel/MediumControlPanel.tsx
@@ -1,33 +1,57 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRecoilState } from 'recoil';
 import ProgressBar from '../../../SmallPlayer/Components/ProgressBar/ProgressBar';
 import styles from './MediumControlPanel.module.scss';
 import { audioPlayerState } from '@/app/Atoms/states';
 import { AudioPlayerPropsInterface } from '@/app/Components/SmallPlayer/interfaces/audio-player-props.interface';
 import { formatTime } from '@/app/Helpers/AudioHelpers';
+import BaseApi from '@/app/api/BaseApi';
 
 const MediumControlPanel = (props: AudioPlayerPropsInterface) => {
-  const [, setAudioPlayer] = useRecoilState(audioPlayerState);
+  const [audioPlayer , setAudioPlayer] = useRecoilState(audioPlayerState);
 
   const handleShuffleClick = () => {
-    setAudioPlayer((prevState) => ({
-      ...prevState,
-      shuffle: !prevState.shuffle,
-    }));
+    setAudioPlayer((prevState) => {
+      const updatedShuffle = !prevState.shuffle;
+      return {
+        ...prevState,
+        shuffle: updatedShuffle,
+      };
+    });
+    BaseApi.post('/music/shuffle',audioPlayer.shuffle)
   };
+
+  useEffect(() => {
+    BaseApi.get('/music')
+  },[])
+
 
   return (
     <div className={styles.progressBar}>
       <div className={styles.icons} onClick={handleShuffleClick}>
-        <Image src={'/shuffle.png'} alt="shuffle icon" width={24} height={24} />
+        {!audioPlayer.shuffle ? (
+          <Image
+            src={'playlist/shuffle.svg'}
+            alt="shuffle icon"
+            width={24}
+            height={24}
+          />
+        ) : (
+          <Image
+            src={'icons/activeShuffle.svg'}
+            alt="shuffle icon"
+            width={24}
+            height={24}
+          />
+        )}
       </div>
       <p>{formatTime(props.currentTime)}</p>
       <ProgressBar
         defaultValue={String((props.currentTime / props.duration) * 100)}
         onChange={props.handleProgressChange}
         progressRef={props.progressRef}
-      />
+      /> 
       <div className={styles.mobileTime}>
         <span>{`${formatTime(props.currentTime)}/${formatTime(props.duration)}`}</span>
       </div>
