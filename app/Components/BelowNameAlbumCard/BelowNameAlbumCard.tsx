b116a8db3c08dd6bfe582a510664ec478f12efbf
sabaabramishvili <sabramashvili02@gmail.com>
2024-09-21 12:58:05 +0400

    refactor/make artist album dynamic

--- app/Components/BelowNameAlbumCard/BelowNameAlbumCard.tsx
+++ app/Components/BelowNameAlbumCard/BelowNameAlbumCard.tsx
@@ -1,13 +1,14 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from './BelowNameAlbumCard.module.scss';
 import { AlbumBelowNamePropsInterface } from './interfaces/album-below-name.props';
 
 const BelowNameAlbumCard = (props: AlbumBelowNamePropsInterface) => {
   return (
-    <div className={styles.container}>
+    <Link className={styles.container} href={`/topalbums/album/${props.id}`}>
       <Image src={props.cover} alt="Artist Photo" height={200} width={220} />
       <span>{props.albumName}</span>
-    </div>
+    </Link>
   );
 };
 
