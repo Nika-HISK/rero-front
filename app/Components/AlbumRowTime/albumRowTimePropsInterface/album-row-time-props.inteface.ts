ee469a1f9ad6167be3bf24f0164ec27c4a5e56ac
sabaabramishvili <sabramashvili02@gmail.com>
2024-09-27 17:37:12 +0400

    fix: add albumrowtime

---
+++ app/Components/AlbumRowTime/albumRowTimePropsInterface/album-row-time-props.inteface.ts
@@ -0,0 +1,15 @@
+import { Song } from '../../SmallPlayer/interfaces/song-props.interface';
+
+export interface AlbumRowTimePropsInterface {
+  isActive: boolean;
+  index: number;
+  artistData: Song;
+  artistDataArray: Song[];
+  src: string;
+  musicId: number;
+  playlistId: number;
+  setArtistData: (id: number) => void;
+  musicAudio: string;
+  isPlaying: boolean;
+  onClick: () => void;
+}
